b42584bcd8585221bc5df4426c97d6ce5273a080
gabriel-g4 <gbgarcia642@gmail.com>
2025-04-29 19:38:27 -0300

    feat: html y css de login hecho

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  msg: string = ""
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit(){
+    if (this.loginForm.valid){
+      this.msg = "valido" 
+    } else {
+      this.msg = "invalido"
+    }
+  }
 }
