03270d99d6eb7b277f82c2d57431a1265b36d6d4
gabriel-g4 <gbgarcia642@gmail.com>
2025-05-03 21:46:30 -0300

    feat: home con grid de juegos

--- src/app/pages/bienvenida/bienvenida.component.ts
+++ src/app/pages/bienvenida/bienvenida.component.ts
@@ -1,32 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubService } from '../../services/github.services';
+import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+  img_url: string
+}
 
 @Component({
   selector: 'app-bienvenida',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './bienvenida.component.html',
   styleUrl: './bienvenida.component.css'
 })
-export class BienvenidaComponent implements OnInit {
-
-  name: string = ""
-  login: string = ""
-  avatar_url: string = ""
-
-  constructor (private githubService : GithubService) {}
-
-  ngOnInit() {
-    this.getGit();
-  }
-
-  getGit () {
-
-    this.githubService.getProfile().subscribe(data => {
-      this.login = data.login;
-      this.name = data.name;
-      this.avatar_url = data.avatar_url;
-      })
-
-    
-  }
+export class BienvenidaComponent {
+  tiles: Tile[] = [
+    {text: 'Ahorcado', cols: 3, rows: 1, color: 'lightblue', img_url: '/juegoahorcado.png'},
+    {text: 'Mayor-menor', cols: 1, rows: 2, color: 'lightgreen', img_url: "/juegomayoromenor.jpg"},
+    {text: 'Preguntados', cols: 1, rows: 1, color: 'lightpink', img_url: "/juegopreguntados.png"},
+    {text: 'Juego propio', cols: 2, rows: 1, color: '#DDBDF1', img_url: "/juegopropio.jpg"},
+  ];
 }
