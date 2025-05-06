00bd966af541a69c57489c131aa9deb495ed5b48
gabriel-g4 <gbgarcia642@gmail.com>
2025-05-06 15:15:09 -0300

    feat: navbar con cambio de tema

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  changeTheme() {
+
+    const html = document.documentElement;
+
+    if (html.getAttribute('data-theme') == 'dark' || (html.getAttribute('data-theme') == undefined && window.matchMedia('(prefers-color-scheme: dark)').matches)) {//== ) {
+      html.setAttribute('data-theme', 'light');
+    } else {
+      html.setAttribute('data-theme', 'dark');
+    }
+  }
 }
