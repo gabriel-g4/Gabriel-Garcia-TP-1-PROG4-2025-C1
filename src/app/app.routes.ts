ae13bb5ecbc3b3f5ec90d6284caa968a61a4c8d0
gabriel-g4 <gbgarcia642@gmail.com>
2025-05-11 19:25:22 -0300

    feat: navegacion de juegos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-import { BienvenidaComponent} from '../app/pages/bienvenida/bienvenida.component';
-import { LoginComponent} from '../app/pages/login/login.component';
-import { QuienSoyComponent} from './pages/quien-soy/quien-soy.component';
-import { RegistroComponent} from '../app/pages/registro/registro.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/bienvenida" , pathMatch: "full" },
-    { path: "bienvenida", component: BienvenidaComponent},
-    { path: "login", component: LoginComponent},
-    { path: "quien-soy", component: QuienSoyComponent},
-    { path: "registro", component: RegistroComponent},
+    { path: "bienvenida", loadComponent: ()=> import('../app/pages/bienvenida/bienvenida.component').then(c => c.BienvenidaComponent) },
+    { path: "login", loadComponent: ()=> import('../app/pages/login/login.component').then(c => c.LoginComponent) },
+    { path: "quien-soy", loadComponent: ()=> import('../app/pages/quien-soy/quien-soy.component').then(c => c.QuienSoyComponent) },
+    { path: "registro", loadComponent: ()=> import('../app/pages/registro/registro.component').then(c => c.RegistroComponent) },
+    { path: "ahorcado", loadComponent: ()=> import('../app/games/ahorcado/ahorcado.component').then(c => c.AhorcadoComponent)},
+    { path: "mayor-menor", loadComponent: ()=> import('../app/games/mayor-menor/mayor-menor.component').then(c => c.MayorMenorComponent)},
+    { path: "preguntados", loadComponent: ()=> import('../app/games/preguntados/preguntados.component').then(c => c.PreguntadosComponent)},
     { path: "**", redirectTo: "/bienvenida", pathMatch: "full"}
 ];
--- src/app/pages/bienvenida/bienvenida.component.ts
+++ src/app/pages/bienvenida/bienvenida.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export interface Tile {
   color: string;
-  cols: number;
-  rows: number;
   text: string;
-  img_url: string
+  img_url: string;
+  url: string;
 }
 
 @Component({
   selector: 'app-bienvenida',
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, RouterLink, RouterLinkActive],
   templateUrl: './bienvenida.component.html',
   styleUrl: './bienvenida.component.css'
 })
 export class BienvenidaComponent {
+
+
   tiles: Tile[] = [
-    {text: 'Ahorcado', cols: 3, rows: 1, color: 'lightblue', img_url: '/juegoahorcado.png'},
-    {text: 'Mayor-menor', cols: 1, rows: 2, color: 'lightgreen', img_url: "/juegomayoromenor.jpg"},
-    {text: 'Preguntados', cols: 1, rows: 1, color: 'lightpink', img_url: "/juegopreguntados.png"},
-    {text: 'Juego propio', cols: 2, rows: 1, color: '#DDBDF1', img_url: "/juegopropio.jpg"},
+    {text: 'Ahorcado', color: 'lightblue', img_url: '/juegoahorcado.png', url: '/ahorcado'},
+    {text: 'Mayor-menor', color: 'lightgreen', img_url: "/juegomayoromenor.jpg", url: '/mayor-menor'},
+    {text: 'Preguntados', color: 'lightpink', img_url: "/juegopreguntados.png", url: '/preguntados'},
+    {text: 'Juego propio', color: '#DDBDF1', img_url: "/juegopropio.jpg", url: ''},
   ];
+
+
 }
