f0c0ef0bccd93c64b1d9ad035bc87253c4973b1e
gabriel-g4 <gbgarcia642@gmail.com>
2025-05-14 21:31:04 -0300

    refactor: cambio de paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,37 +10,37 @@ export const routes: Routes = [
     },
     { 
         path: "bienvenida", 
-        loadComponent: ()=> import('../app/pages/bienvenida/bienvenida.component').then(c => c.BienvenidaComponent)
+        loadComponent: ()=> import('./components/pages/bienvenida/bienvenida.component').then(c => c.BienvenidaComponent)
     },
     { 
         path: "login", 
-        loadComponent: ()=> import('../app/pages/login/login.component').then(c => c.LoginComponent),
+        loadComponent: ()=> import('./components/pages/login/login.component').then(c => c.LoginComponent),
         canActivate: [noAuthGuard]
     },
     { 
         path: "quien-soy", 
-        loadComponent: ()=> import('../app/pages/quien-soy/quien-soy.component').then(c => c.QuienSoyComponent) 
+        loadComponent: ()=> import('./components/pages/quien-soy/quien-soy.component').then(c => c.QuienSoyComponent) 
     },
     { 
         path: "registro", 
-        loadComponent: ()=> import('../app/pages/registro/registro.component').then(c => c.RegistroComponent), 
+        loadComponent: ()=> import('./components/pages/registro/registro.component').then(c => c.RegistroComponent), 
         canActivate: [noAuthGuard]
     },
     { 
         path: "ahorcado", 
-        loadComponent: ()=> import('../app/games/ahorcado/ahorcado.component').then(c => c.AhorcadoComponent),
+        loadComponent: ()=> import('./components/games/ahorcado/ahorcado.component').then(c => c.AhorcadoComponent),
         canActivate: [authGuard]
 
     },
     { 
         path: "mayor-menor", 
-        loadComponent: ()=> import('../app/games/mayor-menor/mayor-menor.component').then(c => c.MayorMenorComponent),
+        loadComponent: ()=> import('./components/games/mayor-menor/mayor-menor.component').then(c => c.MayorMenorComponent),
         canActivate: [authGuard]
 
     },
     { 
         path: "preguntados", 
-        loadComponent: ()=> import('../app/games/preguntados/preguntados.component').then(c => c.PreguntadosComponent),
+        loadComponent: ()=> import('./components/games/preguntados/preguntados.component').then(c => c.PreguntadosComponent),
         canActivate: [authGuard]
 
     },
